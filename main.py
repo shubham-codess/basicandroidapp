c1454731ca359972d6aa299e4849797e29d6c877
Shubham Varshney <shubham.varshney696@gmail.com>
2022-03-27 16:48:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from matplotlib.pyplot import text
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class ShubhamGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(ShubhamGrid,self).__init__()
+        self.cols = 2
+        self.add_widget(Label(text = "Student Name:"))
+        self.s_name = TextInput()
+        self.add_widget(self.s_name)
+        
+        self.add_widget(Label(text = "Student Marks:"))
+        self.s_marks = TextInput()
+        self.add_widget(self.s_marks)
+
+        self.add_widget(Label(text = "Student Gender:"))
+        self.s_gender = TextInput()
+        self.add_widget(self.s_gender)
+
+        self.press = Button(text = "Click Me")
+        self.press.bind(on_press = self.ClickMe)
+        self.add_widget(self.press)
+
+    def ClickMe(self, instance):
+        print("Details Submitted Successfully!")
+        print("Name: " + self.s_name.text)
+        print("Marks: " + self.s_marks.text)
+        print("Gender: " + self.s_gender.text)
+
+
+
+class ShubhamApp(App):
+
+    def build(self):
+
+        return ShubhamGrid()
+
+if __name__ == "__main__":
+    ShubhamApp().run()
